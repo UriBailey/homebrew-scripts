dd33d911fb8d3c59f6e649e9f42b87102755f566
UriBailey <uriba@wix.com>
2026-01-15 14:12:38 +0200

    create rb

---
+++ formula/uri-build.rb
@@ -0,0 +1,30 @@
+class UriBuild < Formula
+  desc "PR rebuild monitor that comments #rebuild when Falcon CI requests it"
+  homepage "https://github.com/"
+
+  url "file:///Users/uriba/scripts/uri-build.sh"
+  version "0.1.0"
+  sha256 :no_check
+
+  depends_on "gh"
+
+  def install
+    bin.install "uri-build.sh" => "uri-build"
+  end
+
+  def caveats
+    <<~EOS
+      Prereqs:
+        - GitHub CLI authenticated: gh auth login
+        - Run inside a git repo with a PR for the branch you monitor
+
+      Usage:
+        uri-build -h
+    EOS
+  end
+
+  test do
+    output = shell_output("#{bin}/uri-build -h", 1)
+    assert_match "Usage:", output
+  end
+end
